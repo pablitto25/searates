2fa4a3e8272dcae70f6d9369a8c3cbd1f86bc187
pablitto25 <pablitto18@gmail.com>
2025-08-07 15:28:27 -0300

    disabled eslint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Ignora ESLint al hacer `next build`
+  },
 };
 
 export default nextConfig;
